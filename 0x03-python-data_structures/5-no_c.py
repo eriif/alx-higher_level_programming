b66eda9273de0250e96b406ca68130692d12bc4e
Eric Karanja <erickaranja54@gmail.com>
2023-08-15 21:18:30 +0300

    data structures

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
+
+
 def no_c(my_string):
-    new_str = ""
-    for i in my_string:
-        if (i != 'c') and (i != 'C'):
-            new_str += i
-    return (new_str)
+    copy = [x for x in my_string if x != 'c' and x != 'C']
+    return ("".join(copy))
--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
+
+
 def max_integer(my_list=[]):
     if len(my_list) == 0:
-        return ("None")
-    x = my_list[0]
-    for i in my_list:
-        if i > x:
-            x = i
-    return (x)
+        return (None)
+
+    big = my_list[0]
+    for i in range(len(my_list)):
+        if my_list[i] > big:
+            big = my_list[i]
+
+    return (big)
