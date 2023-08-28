fd1b7b024775e6d5cc9321bd8952c2eb1fe3981d
Eric Karanja <erickaranja54@gmail.com>
2023-08-29 00:34:40 +0300

    safe print list

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+def safe_print_list(my_list=[], x=0):
+    try:
+        printed = 0
+        for i in range(x):
+            print(my_list[i], end="")
+            printed += 1
+    except IndexError:
+        pass
+    finally:
+        print()
+        return printed
