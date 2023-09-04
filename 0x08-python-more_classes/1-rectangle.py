dfc1e055755adf98c6407f9dac7c4d19f66abe6f
Eric Karanja <erickaranja54@gmail.com>
2023-09-04 23:17:28 +0300

    python classes

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -2,16 +2,8 @@
 
 class Rectangle:
     def __init__(self, width=0, height=0):
-        self.__width = width
         self.__height = height
-
-    @property
-    def width(self):
-        return self.__width
-
-    @width.setter
-    def width(self, value):
-        self.__width = value
+        self.__width = width
 
     @property
     def height(self):
@@ -20,3 +12,11 @@ class Rectangle:
     @height.setter
     def height(self, value):
         self.__height = value
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        self.__width = value
