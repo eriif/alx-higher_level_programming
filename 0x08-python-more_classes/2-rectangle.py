b9e4f255e072a646edf50234e1df7f239cac4a10
Eric Karanja <erickaranja54@gmail.com>
2023-09-06 13:53:37 +0300

    area perimeter

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -9,17 +9,18 @@ class Rectangle:
     def width(self):
         return self.__width
 
+    @property
+    def height(self):
+        return self.__height
+
     @width.setter
     def width(self, value):
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         elif value < 0:
             raise ValueError("width must be >= 0")
-        self.__width = value
-
-    @property
-    def height(self):
-        return self.__height
+        else:
+            self.__width = value
 
     @height.setter
     def height(self, value):
@@ -27,7 +28,8 @@ class Rectangle:
             raise TypeError("height must be an integer")
         elif value < 0:
             raise ValueError("height must be >= 0")
-        self.__height = value
+        else:
+            self.__height = value
 
     def area(self):
         return self.__width * self.__height
@@ -35,4 +37,5 @@ class Rectangle:
     def perimeter(self):
         if self.__width == 0 or self.__height == 0:
             return 0
-        return 2 * (self.__width + self.__height)
+        else:
+            return 2 * (self.__width + self.__height)
