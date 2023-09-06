7f7e22045fe10c0284d6b6ba3efb165e27557790
Eric Karanja <erickaranja54@gmail.com>
2023-09-07 00:40:01 +0300

    classes in python

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -1,23 +1,36 @@
 #!/usr/bin/python3
-""" Rectangle"""
+"""short desc"""
 
 
 class Rectangle:
-    """a class Rectangle that defines a rectangle """
+    """a class Rectangle that defines a rectangle"""
 
     def __init__(self, width=0, height=0):
         """ the initialization """
         self.width = width
         self.height = height
 
+    def __str__(self):
+        """returns an informal string rep"""
+        if self.__height == 0 or self.__width == 0:
+            return ''
+        _str = ''
+        for x in range(self.__height):
+            for y in range(self.__width):
+                _str += '#'
+            _str += '\n'
+        return (_str[:-1])
+
+    def __repr__(self):
+        """return internal string rep"""
+        return ("Rectangle({}, {})".format(self.__width, self.__height))
+
     @property
     def width(self):
-        """ gets the with of the rec from ^^ def"""
-        return (self.__width)
+        return self.__width
 
     @width.setter
     def width(self, value):
-        """Sets the width """
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
@@ -26,15 +39,11 @@ class Rectangle:
 
     @property
     def height(self):
-        """ now it gets the hight"""
-        return (self.__height)
+        return self.__height
 
     @height.setter
     def height(self, value):
-        """
-        Sets the instance of the hight
-        value of the height, must be a positive integer
-        """
+        """ sets the height """
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
@@ -42,18 +51,9 @@ class Rectangle:
         self.__height = value
 
     def area(self):
-        """
-        now i will be calculating the area
-        the fomulae is given by height * width
-        """
-        return (self.__width * self.__height)
+        return self.__width * self.__height
 
     def perimeter(self):
-        """
-        now am geting the perimeter
-        the fomulae is given by 2 * (height + width) for all sides
-        if length && width = 0 : 0
-        """
         if self.__height == 0 or self.__width == 0:
-            return (0)
+            return 0
         return (2 * (self.__width + self.__height))
