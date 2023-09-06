2f45ffc2b12c7e6f8694622156f635e9e97c08ed
Eric Karanja <erickaranja54@gmail.com>
2023-09-07 00:14:00 +0300

    happy coding

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -4,18 +4,30 @@
 
 class Rectangle:
     def __init__(self, width=0, height=0):
+        """
+        Initialize private attributes for width and height
+        """
         self.__width = width
         self.__height = height
 
     @property
+    """
+    Getter method for width attribute
+    """
     def width(self):
         return self.__width
 
     @property
+    """
+    Getter method for height attribute
+    """
     def height(self):
         return self.__height
 
     @width.setter
+    """
+    Setter method for width attribute
+    """
     def width(self, value):
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
@@ -25,6 +37,9 @@ class Rectangle:
             self.__width = value
 
     @height.setter
+    """
+    Setter method for height attribute
+    """
     def height(self, value):
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
@@ -34,9 +49,15 @@ class Rectangle:
             self.__height = value
 
     def area(self):
+        """
+        Calculate and return the area of the rectangle
+        """
         return self.__width * self.__height
 
     def perimeter(self):
+        """
+        Calculate and return the perimeter of the rectangle
+        """
         if self.__width == 0 or self.__height == 0:
             return 0
         else:
