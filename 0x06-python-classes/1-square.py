9b89aa9c0c33a7ea2004714d4c836577b9f32f37
Eric Karanja <erickaranja54@gmail.com>
2023-09-08 00:56:30 +0300

    happy coding

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Define a class Square."""
+
+
+class Square:
+    """
+    Having fun with classes in python
+    """
+    def __init__(self, size):
+        """
+        Initialize a new Square.
+        """
+        self.__size = size
