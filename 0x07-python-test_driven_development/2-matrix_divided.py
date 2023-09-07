5e39eaf56023561d7934b19f7eed57a46b5da7d6
Eric Karanja <erickaranja54@gmail.com>
2023-09-07 23:25:13 +0300

    print square

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""this function divides all elements of a matrix"""
 
 
 def matrix_divided(matrix, div):
@@ -15,20 +16,35 @@ def matrix_divided(matrix, div):
     Returns:
     A new matrix representing the result of the division.
     """
-    if (not isinstance(matrix, list) or matrix == [] or
-            not all(isinstance(row, list) for row in matrix) or
-            not all((isinstance(ele, int) or isinstance(ele, float))
-                    for ele in [num for row in matrix for num in row])):
-        raise TypeError("matrix must be a matrix (list of lists) of"
-                        "integers/floats")
+    if not isinstance(matrix, list) or len(matrix) == 0 or not matrix[0]:
+        raise TypeError("matrix must be a matrix (list of lists) " +
+                        "of integers/floats")
+    if not matrix or not matrix[0]:
+        raise ValueError("matrix cannot be empty")
 
-    if not all(len(row) == len(matrix[0]) for row in matrix):
+    for row in matrix:
+        if not all(isinstance(col, (int, float)) for col in row):
+            raise TypeError("matrix must contain only integers or floats")
+        if len(row) == 0:
+            raise TypeError("matrix must be a matrix (list of lists) " +
+                            "of integers/floats")
+        for col in row:
+            if type(col) is not int and type(col) is not float:
+                raise TypeError("matrix must be a matrix (list of lists) " +
+                                "of integers/floats")
+
+    rows_L = []
+    for row in matrix:
+        rows_L.append(len(row))
+    if not all(col == rows_L[0] for col in rows_L):
         raise TypeError("Each row of the matrix must have the same size")
 
-    if not isinstance(div, int) and not isinstance(div, float):
+    if type(div) is not int and type(div) is not float:
         raise TypeError("div must be a number")
 
     if div == 0:
         raise ZeroDivisionError("division by zero")
 
-    return ([list(map(lambda x: round(x / div, 2), row)) for row in matrix])
+    new_matrix = [[round(col / div, 2) for col in row] for row in matrix]
+
+    return (new_matrix)
---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+def print_square(size):
+    """Print a square with the # character.
+
+    Args:
+    size (int): The height/width of the square.
+    Raises:
+    TypeError: If size is not an integer.
+    ValueError: If size is < 0
+    """
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    if isinstance(size, float) and size < 0:
+        raise TypeError("size must be an integer")
+    """the square constraction"""
+    for x in range(size):
+        for y in range(size):
+            print("#", end="")
+        print()
