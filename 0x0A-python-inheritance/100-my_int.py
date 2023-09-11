109723d1e862ee65261da423ed06bdb3138ea855
Eric Karanja <erickaranja54@gmail.com>
2023-09-12 00:27:41 +0300

    inheritance in python

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Contains the class MyInt
+"""
+
+
+class MyInt(int):
+    """
+    Rebel version of an integer, perfect for opposite day!
+    """
+    def __new__(cls, *args, **kwargs):
+        """
+        Create a new instance of the class
+        """
+        return super(MyInt, cls).__new__(cls, *args, **kwargs)
+
+    def __eq__(self, other):
+        """
+        What was != is now ==
+        """
+        return int(self) != other
+
+    def __ne__(self, other):
+        """
+        What was == is now !=
+        """
+        return int(self) == other
