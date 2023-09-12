4796fdc946d55c5ac6ab54404a072bbaa0b4d890
Eric Karanja <erickaranja54@gmail.com>
2023-09-13 02:02:52 +0300

    append write

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 
-
 def append_write(filename="", text=""):
-    with open(filename, "a", encoding="utf-8") as f:
-        return f.write(text)
+    try:
+        with open(filename, 'a', encoding='utf-8') as file:
+            file.write(text)
+            return len(text)
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+        return 0
