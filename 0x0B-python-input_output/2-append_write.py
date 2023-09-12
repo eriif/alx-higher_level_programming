462faaa90c073be2c62886cca327b9cf92836034
Eric Karanja <erickaranja54@gmail.com>
2023-09-13 02:10:34 +0300

    i/o

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -3,8 +3,12 @@
 def append_write(filename="", text=""):
     try:
         with open(filename, 'a', encoding='utf-8') as file:
-            file.write(text)
-            return len(text)
+            characters_added = file.write(text)
+            return characters_added
     except Exception as e:
         print(f"An error occurred: {str(e)}")
         return 0
+
+if __name__ == "__main__":
+    nb_characters_added = append_write("file_append.txt", "This School is so cool!\n")
+    print(nb_characters_added)
