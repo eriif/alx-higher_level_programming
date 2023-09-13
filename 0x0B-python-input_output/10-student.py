42bc4a86f15c5a77d9900adf6e4907bbd17085f2
Eric Karanja <erickaranja54@gmail.com>
2023-09-14 01:58:25 +0300

    add to json

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -29,7 +29,7 @@ class Student:
         Args:
         attrs (list): (Optional) The attributes to represent.
         """
-    if (type(attrs) == list and
-            all(type(ele) == str for ele in attrs)):
-        return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
-    return self.__dict__
+        if (type(attrs) == list and
+                all(type(ele) == str for ele in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
