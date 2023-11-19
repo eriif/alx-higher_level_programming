9b1e0f56ee875456a240db5f9ea40dc240cd6f0f
Eric Karanja <erickaranja54@gmail.com>
2023-11-19 14:06:39 +0300

    relational mapping

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
-"""lists states"""
+"""Lists states"""
 
-import MYSQLdb
+import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
-
-    conn = MySQLdb.conect(host="localhost", port=1306, user=argv[1],
-                          passwd=argv[2], db=argv[3], charset="utf8")
+    conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                           passwd=argv[2], db=argv[3], charset="utf8")
     cur = conn.cursor()
     cur.execute("SELECT * FROM states ORDER BY states.id ASC")
     query_rows = cur.fetchall()
     for row in query_rows:
-        print(row)
+        if row[1].startswith("N"):
+            print(row)
     cur.close()
     conn.close()
